02b81e874f3f0d74131105f46e355ac6bf6f9ed2
grioool <olga.a.grigorieva@gmail.com>
2021-11-15 10:53:47 +0300

    Initial commit

--- src/main/java/com/epam/training/jwd/online/shop/service/validator/Validator.java
+++ src/main/java/com/epam/training/jwd/online/shop/service/validator/Validator.java
@@ -1,9 +1,6 @@
 package com.epam.training.jwd.online.shop.service.validator;
 
-import com.sun.xml.internal.ws.client.RequestContext;
-
 import java.util.Set;
 
 public interface Validator {
-    Set<String> validate(RequestContext context);
 }
--- src/main/java/com/epam/training/jwd/online/shop/service/validator/impl/UsernameValidator.java
+++ src/main/java/com/epam/training/jwd/online/shop/service/validator/impl/UsernameValidator.java
@@ -1,11 +1,5 @@
 package com.epam.training.jwd.online.shop.service.validator.impl;
 
-import com.epam.training.jwd.online.shop.service.validator.AbstractValidator;
-import com.epam.training.jwd.online.shop.service.validator.Validator;
-import com.sun.xml.internal.ws.client.RequestContext;
-
-import java.util.Set;
-
 public class UsernameValidator {
 }
 
